a5d28685fd0461000983038a0fb99aecd9676e59
Wpaswan <dablumcapaswan@gmail.com>
2021-12-12 09:04:34 -0800

    [I, added code to validate user first name]

---
+++ UserRegistration/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class Registration
+    {
+        
+            public static string FirsName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
+            public bool ValidateUserName(string uName)
+            {
+                return Regex.IsMatch(uName, FirsName);
+            }
+        
+    }
+}
